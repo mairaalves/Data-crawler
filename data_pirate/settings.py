10192ee6003736aede48b8fda9d76cbb18c3410b
mairaalves <marry.alves@hotmail.com>
2020-10-26 21:49:45 -0300

    navigating through result pages and collecting data

--- data_pirate/settings.py
+++ data_pirate/settings.py
@@ -7,3 +7,6 @@ NEWSPIDER_MODULE = 'data_pirate.spiders'
 
 ROBOTSTXT_OBEY = True
 
+FEED_EXPORT_ENCODING = 'utf-8'
+
+LOG_LEVEL = 'INFO'
--- data_pirate/spiders/mail_spider.py
+++ data_pirate/spiders/mail_spider.py
@@ -16,22 +16,40 @@ class MailSpider(scrapy.Spider):
             yield scrapy.FormRequest.from_response(
                 response,
                 formdata={'UF': uf},
-                callback=self.parse_content_page
+                callback=self.parse_content_page,
+                cb_kwargs= dict(uf=uf, index=0)
             )
         
-    def parse_content_page(self, response):
-        counter = 0
-        uf = response.css('table.tmptabela tr td::text').get()
-        table = response.css('table.tmptabela')[1]
+    def parse_content_page(self, response, uf, index):
+        table_list = response.css('table.tmptabela')
+        table = table_list[0]
+
+        if len(table_list) == 2:
+            table = table_list[1]
+
         table_items = table.css('tr')
-        table_items = table_items[2:]
+        table_items = table_items[1:]
         
+        result_url = 'http://www.buscacep.correios.com.br/sistemas/buscacep/ResultadoBuscaFaixaCEP.cfm'
+        next_page_form = response.css('[name="Proxima"] input::attr(value)').getall()
+
         for item in table_items:
-            counter = counter + 1
+            index = index + 1
             yield {
-                'id': f'{uf}-{counter}',
+                'id': f'{uf}-{index}',
                 'localidade': item.css('td::text').get(),
                 'faixa de cep': item.css('td:nth-child(2)::text').get()
             }
 
-
+        if len(next_page_form) > 0:
+            yield scrapy.FormRequest.from_response(
+                response,
+                url = result_url,
+                formdata={
+                    'UF': uf, 'Localidade': next_page_form[1],
+                    'Bairro': next_page_form[2], 'qtdrow': next_page_form[3],
+                    'pagini': next_page_form[4], 'pagfim': next_page_form[5]
+                },   
+                callback=self.parse_content_page,
+                cb_kwargs= dict(uf=uf, index=index)
+            )
